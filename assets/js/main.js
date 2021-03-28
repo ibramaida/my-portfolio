46fe227b3d4efaf36597dab1c125135a3f2a1e57
Ibrahim Maida <ibrahim@o365.tijdelijkehosting.nl>
2021-03-28 23:53:05 +0200

    addedd modal functionality / modal.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,21 +9,24 @@ icon.addEventListener('click', () => {
     navbar.classList.toggle('open')
 } )
 
-
-navbar.addEventListener(
-    'touchstart',
-     e => (touchStart = e.targetTouches[0].clientX)
-)
-navbar.addEventListener(
-    'touchmove',
-    e => (touchEnd = e.targetTouches[0].clientX)
-)
-
-navbar.addEventListener('touchend', e => {
-    if (touchStart - touchEnd > 45) {
-        navbar.classList.remove("open")
-    }
-})
 function closeMenu(e){
     e.classList.toggle("close");
 }
+
+
+
+// navbar.addEventListener(
+//     'touchstart',
+//      e => (touchStart = e.targetTouches[0].clientX)
+// )
+// navbar.addEventListener(
+//     'touchmove',
+//     e => (touchEnd = e.targetTouches[0].clientX)
+// )
+
+// navbar.addEventListener('touchend', e => {
+//     if (touchStart - touchEnd > 45) {
+//         navbar.classList.remove("open")
+//     }
+// })
+
