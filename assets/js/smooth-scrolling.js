85228a91f032bfc2156bc86b2de07e3671f5fe3c
Ibrahim Maida <ibrahim@o365.tijdelijkehosting.nl>
2021-04-02 14:48:58 +0200

    added active link item switching

--- assets/js/smooth-scrolling.js
+++ assets/js/smooth-scrolling.js
@@ -9,4 +9,20 @@ $(document).ready(function() {
         }, 1000);
 
     });
+
+    // active link item switching
+
+    $(window).scroll(function() {
+        var scrollBarLocation = $(this).scrollTop();
+
+        scrollLink.each(function() {
+
+            var sectionOffset = $(this.hash).offset().top - 40;
+
+            if (sectionOffset <= scrollBarLocation) {
+                $(this).parent().addClass('active');
+                $(this).parent().siblings().removeClass('active');
+            }
+        })
+    })
 });
